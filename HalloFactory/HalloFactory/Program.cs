78cf254c7f62d2f77ed61856191c551aef11957c
André <AndreR@ppedv.de>
2023-04-26 10:30:16 +0200

    Factory

---
+++ HalloFactory/HalloFactory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Common;
+
+Console.WriteLine("Hello Factory");
+
+string conString = "Server=(localdb)\\mssqllocaldb;Database=Northwnd;Trusted_Connection=true;TrustServerCertificate=true";
+//string conString = "Server=.;Database=Northwind;Trusted_Connection=true;";
+//DbProviderFactory factory = Microsoft.Data.SqlClient.SqlClientFactory.Instance;
+DbProviderFactory factory = FirebirdSql.Data.FirebirdClient.FirebirdClientFactory.Instance;
+
+DbConnection con = factory.CreateConnection();
+con.ConnectionString = conString;
+con.Open();
+Console.WriteLine("Datenbankverbindung wurde hergestellt");
+
+DbCommand dbCommand = factory.CreateCommand();
+dbCommand.Connection = con;
+dbCommand.CommandText = "SELECT COUNT(*) FROM Employees";
+
+var count = (int)dbCommand?.ExecuteScalar();
+
+Console.WriteLine($"{count} Employees in DB");
+
+
+
