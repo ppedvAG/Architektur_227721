b170a1b434ad300e5eebd950817b81e200d2a80c
André <AndreR@ppedv.de>
2023-04-26 11:12:49 +0200

    Builder build Schränke

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,8 @@
+﻿using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+
+var schrank1 = new Schrank.Builder()
+                          .SetTüren(3)
+                          .SetBöden(5)
+                          .Create();
