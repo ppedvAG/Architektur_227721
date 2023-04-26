48506e9be6bde9a47f9ba32eb0f8eecb4aaf64a2
André <AndreR@ppedv.de>
2023-04-26 10:02:48 +0200

    Der Singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,38 @@
+﻿namespace HalloSingelton
+{
+    internal class Logger
+    {
+        private static Logger instance;
+        private static object instanceLock = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                    lock (instanceLock)
+                    {
+                        if (instance == null)
+                            instance = new Logger();
+                    }
+
+                return instance;
+            }
+        }
+
+        private Logger()
+        {
+            Console.WriteLine("Logger gestartet");
+        }
+
+        public void Info(string msg)
+        {
+            Console.WriteLine($"[INFO] {DateTime.Now:G} {msg}");
+        }
+
+        public void Error(string msg)
+        {
+            Console.WriteLine($"[ERROR] {DateTime.Now:G} {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,15 @@
+﻿using HalloSingelton;
+
+Console.WriteLine("Hello, World!");
+
+//var logger = new Logger();
+
+for (int i = 0; i < 20; i++)
+{
+    Task.Run(() => Logger.Instance.Info(i.ToString()));
+}
+
+Logger.Instance.Info("Moin Logger");
+Logger.Instance.Error("PANIK!");
+
+Logger.Instance.Info("Mehr Logger");
