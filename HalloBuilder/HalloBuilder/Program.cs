e560e42cca2ffced4117484c2eeb719366c57849
André <AndreR@ppedv.de>
2023-04-26 11:54:58 +0200

    Decoratorpattern

--- HalloBuilder/HalloBuilder/Program.cs
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -6,3 +6,10 @@ var schrank1 = new Schrank.Builder()
                           .SetTüren(3)
                           .SetBöden(5)
                           .Create();
+
+var schrank2 = new Schrank.Builder()
+                          .SetTüren(3)
+                          .SetBöden(5)
+                          .SetOberfläche(Schrankoberfläche.Lackiert)
+                          .SetFarbe("blau")
+                          .Create();
